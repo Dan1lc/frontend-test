b2ce2204a99afc6b077dc1d378f749da650a672d
Danil Zelenin <danil110980@mail.ru>
2023-08-02 16:06:36 +0500

    add new eslint rules and add script to package json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,45 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': [ 'error', 2 ],
+    'camelcase':[ 'error', {
+      properties: 'always',
+    } ],
+    'semi':[ 'error', 'always' ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    'quotes': [ 'error', 'single' ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'object-curly-newline': [ 'error', {
+      'ObjectExpression':{
+        'minProperties': 1,
+      },
+      'ImportDeclaration': 'never',
+      'ExportDeclaration': 'never',
+    } ],
+    'object-property-newline': [ 'error', {
+      'allowAllPropertiesOnSameLine': false,
+    } ],
+    'comma-spacing': [ 'error', {
+      'before': false,
+    } ],
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+};
