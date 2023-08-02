f5384ac38b7844ab549b517c9fd77b153879e19f
Danil Zelenin <danil110980@mail.ru>
2023-08-02 16:08:27 +0500

    add aliases

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src/'))
+      // vue
+      .set('@pages', path.resolve(__dirname, 'src/pages/'))
+      .set('@components', path.resolve(__dirname, 'src/components/'))
+      .set('@ui', path.resolve(__dirname, 'src/components/_UiComponents/'))
+      //  utils
+      .set('@hooks', path.resolve(__dirname, 'src/hooks/'))
+      .set('@helpers', path.resolve(__dirname, 'src/helpers/'))
+      .set('@validation', path.resolve(__dirname, 'src/helpers/validation'))
+      //  store
+      .set('@modules', path.resolve(__dirname, 'src/store/modules/'))
+      // assets
+      .set('@styles', path.resolve(__dirname, 'src/assets/styles/'))
+      .set('@images', path.resolve(__dirname, 'src/assets/images/'))
+      .set('@icons', path.resolve(__dirname, 'src/assets/icons/'))
+      .set('@fonts', path.resolve(__dirname, 'src/assets/fonts/'));
+  },
+});
