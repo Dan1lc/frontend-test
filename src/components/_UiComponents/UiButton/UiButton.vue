4c72e570dd7a346351547b9b3506f78a3e544e6d
Danil Zelenin <danil110980@mail.ru>
2023-08-03 12:55:55 +0500

    create ui button

---
+++ src/components/_UiComponents/UiButton/UiButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="ui-button"
+    :class="classes"
+    :disabled="disabled"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  secondary: {
+    type: Boolean,
+    default: () => false,
+  },
+  disabled: {
+    type: Boolean,
+    default: () => false,
+  },
+});
+
+const classes = computed(() => ({
+  'ui-button--secondary':props.secondary,
+}));
+</script>
+
+<style lang="scss" src="./UiButton.scss" scoped/>
--- src/pages/UiPage/UiPage.vue
+++ src/pages/UiPage/UiPage.vue
@@ -1,7 +1,46 @@
 <template>
   <div class="ui-page">
-    ui-page
+    <ul class="ui-page__list">
+      <li>
+        <UiButton @click="testClick">
+          Сохранить
+        </UiButton>
+      </li>
+      <li>
+        <UiButton
+          disabled
+          @click="testClick"
+        >
+          Сохранить
+        </UiButton>
+      </li>
+      <li>
+        <UiButton
+          secondary
+          @click="testClick"
+        >
+          Отменить
+        </UiButton>
+      </li>
+      <li>
+        <UiButton
+          disabled
+          secondary
+          @click="testClick"
+        >
+          Отменить
+        </UiButton>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+import UiButton from '@ui/UiButton/UiButton';
+
+const testClick = () => {
+  console.log('testClick');
+};
+</script>
 
 <style lang="scss" src="./UiPage.scss" scoped/>
