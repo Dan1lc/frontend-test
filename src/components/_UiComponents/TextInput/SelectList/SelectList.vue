7d24b8d3fc9d9130f3610aa752f90a06e479a26c
Danil Zelenin <danil110980@mail.ru>
2023-08-03 17:30:10 +0500

    create SelectList

---
+++ src/components/_UiComponents/TextInput/SelectList/SelectList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul
+    class="select-list"
+    @click.stop
+  >
+    <li
+      v-for="(item,k) in selectArray"
+      :key="k"
+      class="select-list__item"
+    >
+      <button
+        class="select-list__button"
+        @click="$emit('select-value', item)"
+      >
+        {{ item }}
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+defineEmits([ 'select-value' ]);
+
+defineProps({
+  selectArray: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<style lang="scss" src="./SelectList.scss" scoped/>
