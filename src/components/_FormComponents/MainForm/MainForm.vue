ab65aad02d1a1817d077043bb73efc3cdc4aa85e
Danil Zelenin <danil110980@mail.ru>
2023-08-04 09:31:39 +0500

    finish MainForm

--- src/components/_FormComponents/MainForm/MainForm.vue
+++ src/components/_FormComponents/MainForm/MainForm.vue
@@ -9,10 +9,16 @@
       </h2>
       <ul class="main-form__list">
         <li>
-          <TextInput placeholder="Наименование" />
+          <TextInput
+            v-model="v$.name.$model"
+            placeholder="Наименование"
+          />
         </li>
         <li>
-          <TextInput placeholder="Шифр" />
+          <TextInput
+            v-model="v$.number.$model"
+            placeholder="Шифр"
+          />
         </li>
       </ul>
       <h2 class="main-form__title">
@@ -20,23 +26,57 @@
       </h2>
       <ul class="main-form__list main-form__list--last">
         <li>
-          <TextInput placeholder="Страна" />
+          <TextInput
+            v-model="v$.country.$model"
+            placeholder="Страна"
+            select
+            :select-array="countriesList"
+            @select-value="setRegionsSelector"
+          />
         </li>
         <li>
-          <TextInput placeholder="Область / Край" />
+          <TextInput
+            v-model="v$.region.$model"
+            placeholder="Область / Край"
+            select
+            :select-array="regionsList"
+            @select-value="setLocalitiesSelector"
+          />
         </li>
         <li>
-          <TextInput placeholder="Населенный пункт" />
+          <TextInput
+            v-model="v$.locality.$model"
+            placeholder="Населенный пункт"
+            select
+            :select-array="localitiesList"
+            @select-value="setStreetsSelector"
+          />
         </li>
         <li>
-          <TextInput placeholder="Улица / Проспект" />
+          <TextInput
+            v-model="v$.street.$model"
+            placeholder="Улица / Проспект"
+            select
+            :select-array="streetsList"
+          />
         </li>
         <li class="main-form__list-item main-form__list-item--double">
-          <TextInput placeholder="Дом" />
-          <TextInput placeholder="Корпус" />
+          <TextInput
+            v-model="v$.house.$model"
+            placeholder="Дом"
+          />
+          <TextInput
+            v-model="v$.frame.$model"
+            placeholder="Корпус"
+          />
         </li>
         <li>
-          <TextInput placeholder="Почтовый индекс" />
+          <TextInput
+            ref="postcode"
+            v-model="v$.postcode.$model"
+            placeholder="Почтовый индекс"
+            number
+          />
         </li>
       </ul>
     </div>
@@ -44,12 +84,15 @@
       <UiButton
         class="main-form__button"
         secondary
+        :disabled="isClearDisable"
+        @click="clearAllValues"
       >
         Отменить
       </UiButton>
       <UiButton
         class="main-form__button"
-        disabled
+        :disabled="v$.$invalid"
+        @click="submitForm"
       >
         Сохранить
       </UiButton>
@@ -60,6 +103,50 @@
 <script setup>
 import TextInput from '@ui/TextInput/TextInput';
 import UiButton from '@ui/UiButton/UiButton';
+import useDadata from '@hooks/useDadata';
+import useForm from '@hooks/useForm';
+import Inputmask from 'inputmask';
+import { computed, onMounted, ref } from 'vue';
+
+const postcode = ref(null);
+
+onMounted(() => {
+  new Inputmask('', {
+    placeholder: '',
+    regex: '\\d\\d\\d\\d\\d\\d',
+    showMaskOnHover: false,
+    showMaskOnFocus: false,
+  }).mask(postcode.value.$refs.input);
+});
+
+const { formData, v$, clearForm } = useForm();
+
+const {
+  countriesList,
+  setRegionsSelector,
+  regionsList,
+  setLocalitiesSelector,
+  localitiesList,
+  setStreetsSelector,
+  streetsList,
+  clearSelectors,
+} = useDadata(formData);
+
+const isClearDisable = computed(() => {
+  return !Object.values(formData).find((item) => item);
+});
+
+const clearAllValues = () => {
+  clearForm();
+  clearSelectors();
+};
+
+const submitForm = async () => {
+  v$.value.$touch();
+  if (!v$.value.$error) {
+    alert('Форма успешно сохранена!');
+  }
+};
 </script>
 
 <style lang="scss" src="./MainForm.scss" scoped/>
