36d17bce0994be98514090f73fbbbacd86f5a643
Danil Zelenin <danil110980@mail.ru>
2023-08-04 08:19:22 +0500

    create useForm hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,78 @@
+import { onMounted, reactive, watch } from 'vue';
+import { required } from '@vuelidate/validators';
+import { useVuelidate } from '@vuelidate/core';
+import debounce from '@helpers/debounce';
+
+export default function useForm() {
+  const formData = reactive({
+    name: '',
+    number: '',
+    country: '',
+    region: '',
+    locality: '',
+    street: '',
+    house: '',
+    frame: '',
+    postcode: '',
+  });
+
+  const formRules = {
+    name: {
+      required,
+    },
+    number: {
+      required,
+    },
+    country: {
+      required,
+    },
+    region: {
+      required,
+    },
+    locality: {
+      required,
+    },
+    street: {
+      required,
+    },
+    house: {
+      required,
+    },
+    frame: {
+      required,
+    },
+    postcode: {
+      required,
+    },
+  };
+
+  const v$ = useVuelidate(formRules, formData);
+
+  const clearForm = () => {
+    Object.assign(formData, {
+      name: '',
+      number: '',
+      country: '',
+      region: '',
+      locality: '',
+      street: '',
+      house: '',
+      frame: '',
+      postcode: '',
+    });
+  };
+
+  watch(formData, debounce((value) => {
+    localStorage.setItem('formData', JSON.stringify(value));
+  }, 300));
+
+  onMounted(() => {
+    Object.assign(formData, JSON.parse(localStorage.getItem('formData')) || {});
+  });
+
+  return {
+    formData,
+    v$,
+    clearForm,
+  };
+}
